2cc89be68ebf11c369419df075f4c7bf220ed378
Damjan15 <stamenovdamjan20@gmail.com>
2022-06-05 02:47:34 -0700

    AboutMe section created

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { Layout } from "../containers"
-import { Hero } from "../components";
+import { AboutMe, Hero } from "../components";
 
 export default function Home() {
   return (
     <Layout title="Damjan Stamenov | Front-End Developer">
       <Hero />
+      <AboutMe />
     </Layout>
   );
 }
